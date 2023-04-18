0a7ce166bbe43586c18c13a8fc30d8d5ccc6325c
Thomas Henriksen <thomas-henriksen12@hotmail.com>
2023-04-18 13:15:18 +0200

    add checkbox styling

--- Nordbo-vue/src/views/UrCapView.vue
+++ Nordbo-vue/src/views/UrCapView.vue
@@ -10,41 +10,41 @@
     <body>
         <div class="wrapper">
             <h1>UR Preperation</h1>
-            <p>How to Install the URCap Needed and Setup Mimic Controller and Nordbo Driver</p>
+            <p class="checkP">How to Install the URCap Needed and Setup Mimic Controller and Nordbo Driver</p>
 
         <div class="checkbox">
             <div class="steps">
-                <h1>1</h1>
+                <h1 id="checkH1">1</h1>
             </div>
             <div class="description">
-                <p>Plug in the USB with the Caps into the robot Teach Pendant</p>
+                <p id="checkP">Plug in the USB with the Caps into the robot Teach Pendant</p>
             </div>
             <input class="check" type="checkbox">
         </div>
 
         <div class="checkbox">
             <div class="steps">
-                <h1>2</h1>
+                <h1 id="checkH1">2</h1>
             </div>
             <div class="description">
-                <p>Install the Nordbo Driver URCap</p>
-                <p>The Nordbo Driver URCap needs to be version 2.4 </p>
+                <p id="checkP">Install the Nordbo Driver URCap</p>
+                <p id="checkP">The Nordbo Driver URCap needs to be version 2.4 </p>
             </div>
             <input class="check" type="checkbox">
         </div>
 
         <div class="checkbox-long">
             <div class="steps">
-                <h1>3</h1>
+                <h1 id="checkH1">3</h1>
             </div>
             <div class="description">
-                <p>Plug in the USB with the Caps into the robot Teach Pendant</p>
+                <p id="checkP">Plug in the USB with the Caps into the robot Teach Pendant</p>
                 <br>
-                <p>Enable the Nordbo Driver in the check box</p>
-                <p>Insert the Mimic Controller IP: 192.168.1.20 (default)</p> 
-                <p>Note: The IP can be changed but needs to be the same as the Mimic Controller provided in the box.</p>
-                <p>Press “Update” to finish</p>
-                <p>Save the UR Installation File (“Save…” > “Save UR Installation File…”</p>
+                <p id="checkP">Enable the Nordbo Driver in the check box</p>
+                <p id="checkP">Insert the Mimic Controller IP: 192.168.1.20 (default)</p> 
+                <p id="checkP">Note: The IP can be changed but needs to be the same as the Mimic Controller provided in the box.</p>
+                <p id="checkP">Press “Update” to finish</p>
+                <p id="checkP">Save the UR Installation File (“Save…” > “Save UR Installation File…”</p>
             </div>
             <input class="check" type="checkbox">
         </div>
@@ -151,6 +151,13 @@
 .check{
     width: 15%;
     height: 30px;
+}
+#checkH1{
+  color: #ffffff;
+  margin-left: 10px;
+}
+#checkP{
+  color: #ffffff;
 }
 
 </style>
