c939d47ccfe33779c9837ad2464fcfad82475316
Kim Le <lekim@live.dk>
2023-04-20 09:21:08 +0200

    my changes

--- Nordbo-vue/src/router/index.js
+++ Nordbo-vue/src/router/index.js
@@ -6,6 +6,7 @@ import ControllerView from '@/views/ControllerView.vue'
 import IrTrackerView from '@/views/IrTrackerView.vue'
 import UrCapView from '@/views/UrCapView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
