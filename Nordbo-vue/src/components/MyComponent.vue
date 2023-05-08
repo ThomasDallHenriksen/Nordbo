c259af32f8bb1c68b21d5cea59a2f83f7351638c
“Simon <sikje96@gmail.com>
2023-05-08 12:22:27 +0200

    slet filer

---
+++ Nordbo-vue/src/components/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>{{ title }}</h1>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyComponent',
+    props: {
+      title: String,
+      message: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* CSS styles for your component go here */
+  </style>
+  
