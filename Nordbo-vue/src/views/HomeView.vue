6063bc87232c0b654ea5e81fb19d52d3d434ea74
“Simon <sikje96@gmail.com>
2023-05-09 10:59:14 +0200

    footer component

--- Nordbo-vue/src/views/HomeView.vue
+++ Nordbo-vue/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script >
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
-import HardwareView from '@/views/HardwareView.vue';
+import HardwareView from '@/views/HardwareView.vue'
+import Footer from '@/components/Footer.vue';
 
 export default {
   data() {
@@ -28,6 +29,7 @@ export default {
       <p id="pDesrciption">Mimic is a software platform that uses a tracking device to track the user’s movements, enabling the user to record and replicate complex movements that are challenging to automate with robots. Mimic records both position and orientation, drastically reducing the time spent programming and reprogramming a robot. Mimic UR is a URCap made for easy installation on Universal Robots and works together with a Tracker from Nordbo Robotics.</p>
       <h2 id="h2Description">Select the amount of time</h2>
       <!-- Icon sprite -->
+      <footer></footer>
       <div class="select">
         <div class="min-5">
           <div class="image"></div>
