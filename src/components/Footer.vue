3b3955706f108777e2a2c721bceb0625cbbb1573
Kim Le <lekim@live.dk>
2023-05-18 10:45:45 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,118 @@
+<template>
+      <div class="Info">
+        <div class="HumanSkill">
+          <h4 id="humanCentered">Human skills,</h4>
+          <p id="humanCentered">machine precision.</p>
+        </div>
+        <div class="Robotics">
+          <div><p>We are an AI & robotics company specialized in No Code software for robots within industrial manufacturing</p></div>
+          <div class="email-icon">
+            <div class="Robotics email--image"></div>
+            <h3>contact@nordbo-robotics.com</h3>
+          </div>
+          <div class="phone-icon">
+            <div class="Robotics phone--image"></div>
+            <h3>+45 81 98 81</h3>
+          </div>
+        </div>
+        <div class="social">
+          <a class="spriteLink" :href="socialLinkFacebook" target="_blank"></a>
+          <a class="spriteLink link--linkedin" :href="socialLinkLinkedin" target="_blank"></a>
+          <a class="spriteLink link--twitter" :href="socialLinkTwitter" target="_blank"></a>
+          <a class="spriteLink link--instagram" :href="socialLinkInstagram" target="_blank"></a>
+        </div>
+      </div>
+      <div class="copyright">
+        <p>Copyright @ Nordbo robotics</p>
+      </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+    data() {
+      return { 
+        socialLinkFacebook: 'https://www.facebook.com/nordborobotics/?locale=da_DK',
+        socialLinkLinkedin: 'https://www.linkedin.com/company/nordbo-robotics/?originalSubdomain=dk',
+        socialLinkTwitter: 'https://twitter.com/nordborobotics?lang=da',
+        socialLinkInstagram: 'https://www.instagram.com/nordbo_robotics/?hl=da'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .Info {
+  background-color: #02215c;
+  width: 100%;
+  height: 10rem;
+}
+
+.Info .HumanSkill #humanCentered {
+  text-align: left;
+  font-family: Helvetica;
+  padding-left: 32%;
+  font-size: 1vw;
+}
+
+.Info .Robotics .email-icon{
+  display: flex;
+}
+
+.Info .Robotics .phone-icon{
+  display: flex;
+}
+
+.Info .Robotics .email-icon h3,
+.Info .Robotics .phone-icon h3 {
+  font-family: Helvetica;
+}
+
+.Info .Robotics .email-icon .email--image{
+  background-image: url('@/assets/images/iconsprite.png') ;
+  background-position: -182px -20px;
+  width: 60px;
+  height: 30px;
+  color: #ffffff;
+  display: flex;
+}
+
+.Info .Robotics .phone-icon .phone--image{
+  background-image: url('@/assets/images/iconsprite.png') ;
+  background-position: -238px -20px;
+  width: 60px;
+  height: 30px;
+  color: #ffffff;
+  display: flex;
+}
+
+.Info .social .spriteLink {
+  display: inline-block;
+  background-position: -308px -87px;
+  width: 60px;
+  height: 60px;
+  background-image: url('@/assets/images/iconsprite.png');
+  margin-top: 10%;
+}
+
+.Info .social .spriteLink.link--linkedin {
+  background-position: -373px -87px;
+}
+
+.Info .social .spriteLink.link--twitter {
+  background-position: -435px -87px;
+}
+
+.Info .social .spriteLink.link--instagram {
+  background-position: -491px -87px;
+}
+
+.Info .copyright p {
+  color: white;
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+
+  </style>
+  
---
+++ src/views/UrPreperationView.vue
@@ -0,0 +1,138 @@
+<script>
+</script>
+<template>
+    <header>
+        <div id="pBar">
+            <progress id="file" value="50.0" max="100"> 50.0% </progress>
+        </div>
+    </header>
+    <body class="wrapper"> 
+        <div class="container">
+              <img class="banner" src="../assets/images/Nordbo-banner.png" alt="" width="1216" height="200" aria-placeholder="Welcome to the Mimic Guide">
+              <div class="banner-text">
+                <h1 id="banner-h1">Mimic Guide</h1>
+              </div>
+            </div>
+        <div>
+            <div>
+                <h1 id="h1Title">UR Preperation</h1>
+                <div id="urIndent">
+                    <p id="urPrepP">In this course you will learn how to prepare robots from Universal Robots (UR) using Mimic</p>
+                    <h1 id="h1TitlePrep">What you need prior to Mimic installation</h1>
+                    <p id="urPrepP">-HD ready touch screen (22" is recommended) or HD ready screen with mouse and keyboard</p>
+                    <p id="urPrepP">-Gigabit switch (optional - needed if connecting other hardware to the robot)</p>
+                    <p id="urPrepP">-ethernet cables long enough to reach from your teaching to the robot </p>
+                </div>
+            </div>
+            <br>
+            <div>
+                <div id="split">
+                    <div>
+                        <br>
+                        <br>
+                        <p id="urPrepP">Mimic is compatible with Universal Robots' Ployscope version: </p>
+                        <p id="urPrepP">- CB Series: 3.14.3 or newer</p>
+                        <p id="urPrepP">- eSeries: 5.9.4 or newer</p>
+                        <p id="urPrepP">Mimic is compatible with all UR robots models</p>
+                    </div>
+                    <div>
+                        <img id="UrPrepImg" src="/src/assets/images/UrPrep.png">
+                    </div>
+                </div>
+            </div>
+            <div class="buttons">
+            <div>
+                <router-link 
+                    class="backButton" 
+                    to="/IrTrackerView"
+                    custom
+                    v-slot="{ navigate }"
+                >
+                    <button
+                    @click="navigate"
+                    role="link"
+                    id="backButton"
+                    >
+                    Back
+                    </button>
+                </router-link>
+            </div>
+            <div>
+                <router-link class="nextButton"
+                    to="/UrSetupView"
+                    custom
+                    v-slot="{ navigate }"
+                >
+                    <button
+                    @click="navigate"
+                    role="link"
+                    id="nextButton"
+                    >
+                    Next
+                    </button>
+                </router-link>
+            </div>
+        </div>
+        </div>
+
+        
+    </body>
+    <footer>
+    <div class="Info">
+    <div class="HumanSkill">
+      <h4 id="humanCentered">Human skills,</h4>
+      <p id="humanCentered">machine precision</p>
+    </div>
+    <div class="Robotics">
+      <div><p>We are an AI & robotics company specializied in No Code software for robots within industrial manufactoring</p></div>
+      <div class="hej">
+        <div class="Robotics email--image"></div>
+        <h3>contact@nordbo-robotics.com</h3>
+      </div>
+      <div class="mo">
+        <div class="Robotics phone--image"></div>
+        <h3>+45 81 98 81</h3>
+      </div>
+    </div>
+    <div class="social">
+      <a class="spriteLink" :href="socialLinkFacebook" target="_blank"></a>
+      <a class="spriteLink link--linkedin" :href="socialLinkLinkedin" target="_blank"></a>
+      <a class="spriteLink link--twitter" :href="socialLinkTwitter" target="_blank"></a>
+      <a class="spriteLink link--instagram" :href="socialLinkInstagram" target="_blank"></a>
+    </div>
+    </div>
+    <div class="copyright">
+      <p>Copyright @ Nordbo robotics</p>
+    </div>
+
+  </footer>
+    <RouterView />
+</template>
+<style>
+#split{
+    display: flex;
+    justify-content: center;
+}
+
+#urPrepP{
+    text-align: left;
+    font-family: Helvetica;
+    margin-top: 1%;
+    font-size: 1.3vw;
+    line-height: 1.1;
+}
+
+#h1TitlePrep{
+  margin-top: 1%;
+  font-size: 2vw;
+  text-align: left;
+}
+
+#UrPrepImg{
+    margin-left: 2%;
+}
+
+#urIndent{
+    padding-left: 10%;
+}
+</style>
